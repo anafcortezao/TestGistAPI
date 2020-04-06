1ae2d763e5e6bbc7fae832ef14ab29b4825d5d5b
anafcortezao <anafcortezao@gmail.com>
2020-04-06 20:00:02 -0300

    Added userName variable

--- src/test/java/utils/Config.java
+++ src/test/java/utils/Config.java
@@ -7,6 +7,9 @@ public class Config {
     //personal git token
     protected String token = "";
 
+    //specified user to list gists
+    protected String userName = "";
+
     //url base to do request on API
     protected String urlBase = "https://api.github.com/";
 }
