b24db48c5a73026d7fb1f01936d45622e9b0c624
anafcortezao <anafcortezao@gmail.com>
2020-04-06 19:58:53 -0300

    Create post request

--- src/test/java/utils/RestInterface.java
+++ src/test/java/utils/RestInterface.java
@@ -1,15 +1,8 @@
 package utils;
 
-import io.cucumber.core.internal.gherkin.Parser;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.*;
-import io.restassured.specification.RequestSpecification;
-/*import org.codehaus.groovy.ast.stmt.BreakStatement;
-import io.restassured.specification.*;
-import io.restassured.response.ResponseBody;
-import org.json.JSONObject;
-import org.testng.Assert;*/
 
 public class RestInterface extends Config{
     private Response response;
@@ -19,29 +12,18 @@ public class RestInterface extends Config{
         RestAssured.baseURI = urlBase;
     }
 
-    public Response postRequest(String bodyRequest,String path) {
-        RequestSpecification request = RestAssured.given();
-        request.contentType(ContentType.JSON);
-        request.auth().oauth2(token);
-        if (bodyRequest!=null){
-            request.body(bodyRequest);
-        }
-        request.post(path);
-
-        /*RestAssured.given()
+    public Response postRequest(String path, String payload) {
+        response = RestAssured.given()
                 .contentType(ContentType.JSON)
                 .auth().oauth2(token)
                 .with()
                 .body(payload)
-                .when().post("/gists");*/
-
-        //response = request.post(RestAssured.baseURI+path);
-
+                .when().post(path);
         return response;
     }
 
-    public Response getListGistsForUser(String user) {
-        response = RestAssured.get(RestAssured.baseURI +"users/"+user+"/gists");
+    public Response getListGistsForUser() {
+        response = RestAssured.get(RestAssured.baseURI +"users/"+userName+"/gists");
         return response;
     }
 
