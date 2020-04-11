1d1ba0f9e4846b33f2df391b9c9634de6a7321ea
anafcortezao <anafcortezao@gmail.com>
2020-04-10 21:59:01 -0300

    Create Request Builder

--- src/test/java/utils/RestInterface.java
+++ src/test/java/utils/RestInterface.java
@@ -1,47 +1,67 @@
 package utils;
 
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.response.*;
+import io.restassured.specification.RequestSpecification;
 
 public class RestInterface extends Config{
     private Response response;
-
+    private static RequestSpecification requestSpecification;
 
     public RestInterface(){
-        RestAssured.baseURI = urlBase;
+
+        requestSpecification = new RequestSpecBuilder()
+                .log(LogDetail.ALL)
+                .setContentType(ContentType.JSON)
+                .setBaseUri(urlBase)
+                .build();
+
     }
 
     public Response postRequest(String path, String payload) {
-        response = RestAssured.given()
-                .contentType(ContentType.JSON)
-                .auth().oauth2(token)
+        response =
+                RestAssured.given().
+                        spec(requestSpecification).
+                        auth().oauth2(token)
                 .with()
-                .body(payload)
-                .when().post(path);
+                    .body(payload)
+                .when()
+                    .post(path);
+
         return response;
     }
 
     public Response getListGistsForUser() {
-        response = RestAssured.get(RestAssured.baseURI +"users/"+userName+"/gists");
+        response =
+                RestAssured.given().
+                    spec(requestSpecification).
+                when().
+                    get("users/"+userName+"/gists");
         return response;
     }
 
     public Response patchRequest(String payload){
-        response = RestAssured.given()
-                .contentType(ContentType.JSON)
-                .auth().oauth2(token)
-                .with()
-                .body(payload)
-                .when().patch("/gists/"+"5bd0375ed5a5a03c698a7b8f8fb3b738");
+        response =
+                RestAssured.given().
+                        spec(requestSpecification).
+                        auth().oauth2(token).
+                with().
+                        body(payload).
+                when().
+                        patch("/gists/"+"5bd0375ed5a5a03c698a7b8f8fb3b738");
         return response;
     }
 
     public  Response deleteRequest(){
-        response = RestAssured.given()
-                .contentType(ContentType.JSON)
-                .auth().oauth2(token)
-                .when().delete("/gists/"+"fbb868285d4b16bdfbf26933683e7242");
+        response =
+                RestAssured.given().
+                        spec(requestSpecification).
+                        auth().oauth2(token)
+                .when().
+                        delete("/gists/"+"daf1afc2f5f8d44d0d62865620fc1261");
         return response;
     }
 
